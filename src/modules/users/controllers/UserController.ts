e2dfc7e200f3e0164d7a1adcecf906ddc4e2290b
Diego Souza <diegomagasouza@gmail.com>
2021-11-09 22:34:15 -0300

    done: module users

---
+++ src/modules/users/controllers/UserController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import CreateUserService from '../services/CreateUserService';
+import ListUserService from '../services/ListUserUserService';
+// import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+class UserController {
+    public async list(request: Request, response: Response): Promise<Response> {
+        const service = new ListUserService();
+        const users = await service.execute();
+        return response.json(users);
+    }
+
+    public async create(
+        request: Request,
+        response: Response,
+    ): Promise<Response> {
+        const { name, email, password } = request.body;
+        const service = new CreateUserService();
+        const user = await service.execute({ name, email, password });
+        // console.log('user');
+        return response.json(user);
+    }
+}
+
+export default UserController;
